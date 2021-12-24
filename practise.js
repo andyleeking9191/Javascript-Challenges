eb742a09dae78bda427931824054d325228a5746
Andrew King <andyleeking9191@yahoo.com>
2021-12-24 13:25:07 -0500

    properly formated

--- practise.js
+++ practise.js
@@ -2,15 +2,14 @@
   Write a program that will number 15 rooms starting from 1 and skipping the number 13. 
   Output to the console each room number in separate line.
   Be attentive - considering the missing number, the number of last room should be greater than the count of rooms by 1.
-*/ 
+*/
 
 const countOfRooms = 15;
 
-    for (i=1; i <= countOfRooms + 1; i++) {
-        if (i == 13){
-            continue;
-        } else {
-            console.log(i)
-        }
-        
+for (i = 1; i <= countOfRooms + 1; i++) {
+    if (i == 13) {
+        continue;
+    } else {
+        console.log(i)
     }
+}
